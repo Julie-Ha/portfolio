eb03116d99822190ad08443308590e02b40444df
Julie-Ha <69715947+Julie-Ha@users.noreply.github.com>
2020-09-13 10:39:47 +0200

    add smooth scroll and hamburger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import React, { useContext, useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 import { DataContext } from "../context";
 import "./Header.css";
+import { Link } from "react-scroll";
 
 function Header() {
   const context = useContext(DataContext);
   const { name, role, networks } = context;
   const [nav, setNav] = useState(false);
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
   const changeBackground = () => {
     if (window.scrollY >= 160) {
@@ -13,28 +19,94 @@ function Header() {
     } else {
       setNav(false);
     }
+    if (window.scrollY >= 700) {
+      setButton(true);
+    } else {
+      setButton(false);
+    }
   };
 
   window.addEventListener("scroll", changeBackground);
 
   return (
     <header id="home">
-      <nav className={nav ? 'nav-wrap active' : 'nav-wrap'}>
-        <ul id="nav" className="nav">
-          <li>
-            <a href="#home">Accueil</a>
+      <nav className={nav ? "nav-wrap active" : "nav-wrap"}>
+        <div
+          className={button ? "menu-icon dark" : "menu-icon light"}
+          onClick={handleClick}
+        >
+          {click ? <FaTimes /> : <FaBars />}
+        </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link
+              className="nav-link"
+              activeClass="active"
+              to="home"
+              spy={true}
+              smooth={true}
+              offset={0}
+              duration={500}
+              onClick={closeMobileMenu}
+            >
+              Accueil
+            </Link>
           </li>
-          <li>
-            <a href="#about">A propos</a>
+          <li className="nav-item">
+            <Link
+              className="nav-link"
+              activeClass="active"
+              to="about"
+              spy={true}
+              smooth={true}
+              offset={0}
+              duration={500}
+              onClick={closeMobileMenu}
+            >
+              A propos
+            </Link>
           </li>
-          <li>
-            <a href="#projects">Projets</a>
+          <li className="nav-item">
+            <Link
+              className="nav-link"
+              activeClass="active"
+              to="projects"
+              spy={true}
+              smooth={true}
+              offset={0}
+              duration={500}
+              onClick={closeMobileMenu}
+            >
+              Projets
+            </Link>
           </li>
-          <li>
-            <a href="#skills">Compétences</a>
+          <li className="nav-item">
+            <Link
+              className="nav-link"
+              activeClass="active"
+              to="skills"
+              spy={true}
+              smooth={true}
+              offset={0}
+              duration={500}
+              onClick={closeMobileMenu}
+            >
+              Compétences
+            </Link>
           </li>
-          <li>
-            <a href="#drawings">Dessins</a>
+          <li className="nav-item">
+            <Link
+              className="nav-link"
+              activeClass="active"
+              to="drawings"
+              spy={true}
+              smooth={true}
+              offset={0}
+              duration={500}
+              onClick={closeMobileMenu}
+            >
+              Dessins
+            </Link>
           </li>
         </ul>
       </nav>
